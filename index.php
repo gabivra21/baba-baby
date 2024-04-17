837abfb2929842235463bd194ac39ff4c4f60a58
niimf <75733381+niimf@users.noreply.github.com>
2024-04-17 10:57:00 -0300

    Update index.php
    front feito

--- index.php
+++ index.php
@@ -1,9 +1,53 @@
-<h1>* USUÁRIOS *</h1>
-<p>Selecione o tipo de conta:</p>
-<tr>
-    <td>
-        <a href="select.php">[ TODOS ] </a>
-        <a href="selectBABA.php">[ BABÁ ] </a>
-        <a href="selectPAIS.php">[ PAIS ]</a>
-    </td>
-</tr>
+<html>
+    <head>
+        <title>BabáBaby</title>
+        <link rel="stylesheet" href="css\style.css">
+    </head>
+    <body>
+    <header>
+            <div class="center">
+                <div class="logo"><img src="imagem\bbbyy.png"></div>
+                <div class="menu">
+                    <ul>
+                        <div class="dropdown">
+                            <li class="dropbtn">Geral</li>
+                            <div class="dropdown-content">
+                                <a href="select.php">Listar</a>
+                            </div>
+                        </div>
+                        
+                        <div class="dropdown">
+                            <li class="dropbtn">Babás</li>
+                            <div class="dropdown-content">
+                                <a href="selectBABA.php">Listar</a>
+                                <a href="cadastrarBABA1.php">Cadastrar</a>
+                            </div>
+                        </div>
+                       
+                        <div class="dropdown">
+                            <li class="dropbtn">Pais</li>
+                            <div class="dropdown-content">
+                                <a href="selectPAIS.php">Listar</a>
+                                <a href="cadastrarPAIS1.php">Cadastrar</a>
+                            </div>
+                        </div>
+                        <div class="cc">
+                        <a href="cadastroPG.php">Criar Conta</a>
+                        </div>
+                    </ul>    
+                </div>  
+            </div>  
+        </header>
+        <section class="sobre">
+            <div class="extras">
+                <img src="">
+            </div>
+            <div class="center">
+                <div class="texto-sobre">
+                    <h1>Confie-nos o <span style="color: darksalmon;">cuidado</span>,<br>liberamos seu <span style="color: rgb(156, 88, 220);">tempo</span>.</h1>
+                    <p>Nosso site conecta pais e babás de forma segura, auxiliando os pais na busca pela babá ideal e permitindo uma avaliação mútua. Oferecemos recursos de segurança e privacidade para garantir um ambiente confiável para a troca de informações pessoais.</p>
+                </div>
+            </div>
+    </body>
+</html>
+
